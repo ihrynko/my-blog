d1098df92665e47128979a1003cb787d842efe8c
Iryna Hrynko <ir.hrynko@gmail.com>
2022-07-04 15:23:02 +0200

    add changes

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/my-blog/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
